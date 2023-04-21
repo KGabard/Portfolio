26dfca5e0ea2f13eb9682f444a68154cf20e34c3
KGabard <kev.gabard@gmail.com>
2023-04-21 17:28:20 +0200

    Ajout des réseaux sociaux dans le footer

--- app/layouts/Footer.tsx
+++ app/layouts/Footer.tsx
@@ -1,4 +1,8 @@
+import Link from 'next/link'
 import LogoFullSVG from '../components/svgs/LogoFullSVG'
+import GithubSVG from '../components/svgs/GithubSVG'
+import LinkedinSVG from '../components/svgs/LinkedinSVG'
+import NpmSVG from '../components/svgs/NpmSVG'
 
 export default function Footer() {
   return (
@@ -7,11 +11,25 @@ export default function Footer() {
       className="my-[112px] flex w-full flex-col gap-12 px-[var(--margin-mobile)] sm:px-[var(--margin-desktop)]"
     >
       <div className="h-2 w-full rounded-sm bg-neutral-3 dark:bg-neutral-dark-2 "></div>
-      <div className='flex flex-col gap-2 items-start'>
+      <div className="flex flex-col items-start gap-2">
         <LogoFullSVG className="h-[55px] fill-black dark:fill-white" />
         <p className="typo-body text-neutral-1 dark:text-neutral-dark-3">
           © 2023 - Design et intégration par Kevin Gabard
         </p>
+        <div className="flex gap-2.5 mt-1 sm:hidden">
+          <Link href={'https://github.com/KGabard'} target="_blank">
+            <GithubSVG className="h-9 w-9 cursor-pointer text-neutral-1 opacity-50 transition-all duration-150 ease-in hover:opacity-100 dark:text-neutral-dark-3 dark:hover:text-white" />
+          </Link>
+          <Link
+            href={'https://www.linkedin.com/in/kevin-gabard/'}
+            target="_blank"
+          >
+            <LinkedinSVG className="h-9 w-9 cursor-pointer text-neutral-1 opacity-50 transition-all duration-150 ease-in hover:opacity-100 dark:text-neutral-dark-3 dark:hover:text-white" />
+          </Link>
+          <Link href={'https://www.npmjs.com/~kgabard'} target="_blank">
+            <NpmSVG className="h-9 w-9 cursor-pointer text-neutral-1 opacity-50 transition-all duration-150 ease-in hover:opacity-100 dark:text-neutral-dark-3 dark:hover:text-white" />
+          </Link>
+        </div>
       </div>
     </footer>
   )
